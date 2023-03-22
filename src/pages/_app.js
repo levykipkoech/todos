918b0de401cb7a65925974420a6286394f0ceed1
Levy <kipkoechlevy096@gmail.com>
2023-03-23 00:09:19 +0300

    created necessary files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,8 @@
-import '@/styles/globals.css'
-import Login from './_login'
-import Create from './_create'
-import Registration from './_register'
+ 
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-    <Routes>
-      <Route path="/login" component={Login} />
-      <Route path="/create" component={Create} />
-      <Route path="/register" component={Registration} />
-     
-    </Routes>
-    </>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
