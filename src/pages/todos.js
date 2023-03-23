baf14db7d246987c7fd580f79ed3f86401a217c2
Levy <kipkoechlevy096@gmail.com>
2023-03-23 20:50:01 +0300

    fetched data

--- src/pages/todos.js
+++ src/pages/todos.js
@@ -1,7 +1,24 @@
-const Todo = () => {
+ export const getStaticProps = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+    const data = await res.json();
+    return {
+        props: { todos: data}
+    }
+ }
+
+
+const Todo = ({todos}) => {
     return ( 
         <div>
-            <h1>Todo</h1>
+            <h1>Your Todos</h1>
+            {todos.map(todo => (
+                <div key={todo.userId}>
+                    <h2>{todo.title}</h2>
+                    <h3>{todo.completed}</h3>
+
+                </div>
+            ))}
         </div>
      );
 }
