41c8da9f982d7a595cc4336e36d7f596d42ab08b
Levy <kipkoechlevy096@gmail.com>
2023-03-24 15:39:18 +0300

    added a sign up functionality

--- src/pages/register.js
+++ src/pages/register.js
@@ -6,15 +6,39 @@ const Registration = () => {
      const[email, setEmail] = useState("")
      const[password,setPassword] = useState("")
 
+     const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const userData = {
+            username: username,
+            email: email,
+            password: password
+        };
+
+        const response = await fetch('/api/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userData)
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+     };
+
     return ( 
         <div>
-            <label>username</label>
-        <input type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} required></input>
-        <label>email</label>
-        <input type="text" placeholder="Enter username" value={email} onChange={e => setEmail(e.target.value)} required></input>
-        <label>password</label>
-        <input type="text" placeholder="Enter username" value={password} onChange={e => setPassword(e.target.value)} required></input>
-       
+            <form onSubmit={handleSubmit}>
+                <label>Username</label>
+                <input type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} required />
+                <label>Email</label>
+                <input type="text" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} required />
+                <label>Password</label>
+                <input type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} required />
+                <button type="submit">Sign Up</button>
+            </form>
         </div>
      );
 }
