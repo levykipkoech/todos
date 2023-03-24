0e3748bddc845db3b5fa714d56b8ba0e2c81b6dc
Levy <kipkoechlevy096@gmail.com>
2023-03-24 15:47:18 +0300

    added sign in functionality

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const[username, setUsername] = useState("")
-     const[email, setEmail] = useState("")
-     const[password,setPassword] = useState("")
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await response.json();
+        console.log(data);
+        // do something with the response, such as redirect the user to a different page
+    };
 
-    return ( 
+    return (
         <div className="login">
-           <label>username</label>
-        <input type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} required></input>
-        <label>email</label>
-        <input type="text" placeholder="Enter username" value={email} onChange={e => setEmail(e.target.value)} required></input>
-        <label>password</label>
-        <input type="text" placeholder="Enter username" value={password} onChange={e => setPassword(e.target.value)} required></input>
-        
+            <label>username</label>
+            <input type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} required></input>
+            <label>email</label>
+            <input type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+            <label>password</label>
+            <input type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} required></input>
+            <button onClick={handleLogin}>Log in</button>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Login;
